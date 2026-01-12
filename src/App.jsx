65621a2c5c1b6fdcfb5fa87d8d940e8210c9a2d2
Hasnainali404 <jeinn467@gmail.com>
2026-01-12 03:18:20 -0800

    refactor: Remove unnecessary useEffect for default hash location

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { useEffect } from "react";
 import "./index.css";
 import AppRouter from "./router/router";
 
 function App() {
-  useEffect(() => {
-      location.hash = "/";
-  }, []);
   return (
     <div className="min-h-screen bg-slate-50 flex flex-col font-sans">
       <AppRouter />
